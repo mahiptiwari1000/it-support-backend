3007a5d0ad003ec125fa3f335f52d1ad625599b2
Mahip Tiwari <mahiptiwari@Mahips-MacBook-Air.local>
2024-12-01 03:37:47 -0600

    Updated schema of Create Ticket

--- models/Ticket.js
+++ models/Ticket.js
@@ -5,14 +5,14 @@ const mongoose = require('mongoose');
 const ticketSchema = new mongoose.Schema({
   arNumber: { type: String, required: true },
   title: { type: String, required: true },
-  description: { type: String, required: true },
-  severity: { type: String, required: true },
-  priority: { type: String, required: true },
-  product: { type: String, required: true },
-  subProduct: { type: String, required: true },
-  status: { type: String, required: true },
-  assignee: { type: String, required: true },
-  assigneeEmail: { type: String, required: true },
+  description: { type: String, required: false },
+  severity: { type: String, required: false },
+  priority: { type: String, required: false },
+  product: { type: String, required: false },
+  subProduct: { type: String, required: false },
+  status: { type: String, required: false },
+  assignee: { type: String, required: false },
+  assigneeEmail: { type: String, required: false },
   userId: { type: String, required: true },
   dateCreated: { type: Date, default: Date.now }
 });
