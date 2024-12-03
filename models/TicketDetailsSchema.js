edf5ec7fa130e52f4632161f0c55396bdbbb2a47
Mahip Tiwari <mahiptiwari@Mahips-MacBook-Air.local>
2024-12-03 01:12:33 -0600

    Ticket status to Assigned by default

--- models/TicketDetailsSchema.js
+++ models/TicketDetailsSchema.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose');
 
-const TicketDetailsSchema = new mongoose.Schema(
-  {
-    arNumber: String,
-    userId: String,
-    status: String,
-    priority: String,
-    severity: String,
-    description: String,
-    progressLog: [String],
-    resolutionNotes: String,
-    attachment: Buffer,
-    attachmentType: String,
-  },
-  { timestamps: true } // Adds `createdAt` and `updatedAt` fields
-);
+const TicketDetailsSchema = new mongoose.Schema({
+  arNumber: { type: String, required: true },
+  priority: { type: String, required: true },
+  severity: { type: String, required: true },
+  status: { type: String, default: 'Assigned' }, // Default status
+  description: { type: String, required: true },
+  progressLog: { type: Array, default: [] },
+  resolutionNotes: { type: String, default: '' },
+  userId: { type: String, required: true },
+  attachment: { type: Buffer, default: null },
+  attachmentType: { type: String, default: null },
+}, { timestamps: true });
 
 module.exports = mongoose.model('TicketDetails', TicketDetailsSchema);
